70851a84a1249fc7d6f1fe70ed48fa29b2dfd481
Marios Toparopoulos <mtoparopoulos@ph.esl-asia.com>
2022-03-09 09:45:30 +0200

    [Resume Inside Out Template] Added SHA as Teacher Assistant

--- src/assets/data/volunteer.js
+++ src/assets/data/volunteer.js
@@ -6,6 +6,13 @@ const items = [
     dateTo: 'present',
   },
   {
+    title: 'Teacher Assistant',
+    dateFrom: 'Oct 2019',
+    dateTo: 'Oct 2020',
+    location: 'Social Hackers Academy',
+    link: 'https://socialhackersacademy.org/',
+  },
+  {
     title: 'Treasurer, Council Member',
     link: 'https://www.adhdhellas.org/2013-09-04-22-54-11/item/444-anakoinosi-gia-ta-apotelesmata-ton-arxairesion-tou-adhd-hellas#:~:text=%CE%A4%CE%BF%CF%80%CE%B1%CF%81%CE%BF%CF%80%CE%BF%CF%85%CE%BB%CE%BF%CF%82%20%CE%9C%CE%AC%CF%81%CE%B9%CE%BF%CF%82',
     location: 'ADHD Hellas',
